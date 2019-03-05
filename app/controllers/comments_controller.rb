055c4d3e2c7483aa20fb7be716ef78f7c6767a99
thyagopcarvalho <thyagopcarvalho@gmail.com>
2019-03-04 21:55:46 -0300

    [FIXED] Correction create comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.new(comment_params.merge(user_id: current_user.id))
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			render template: 'posts/show', locals: {post: @post, comment: @comment }
+			# render 'new'
+		end
+	end
+
+	private
+
+	def comment_params
+		params[:comment].permit(:content)
+	end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show,:edit,:update,:destroy]
+	before_action :authenticate_user!
 	def index
 		@posts = Post.all.order("created_at DESC")
 	end
@@ -10,7 +11,6 @@ class PostsController < ApplicationController
 	def new
 		@post = current_user.posts.build
 	end
-
 
 	def create
 		@post = current_user.posts.build(post_params)
